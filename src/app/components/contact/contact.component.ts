1ec4df3f4062678730befe6fe1a56fb8b8542009
qxz54xo <mahmoud.alghazzawi@partner.bmwgroup.com>
2025-10-10 17:20:35 +0200

    small fixes

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
-import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -35,7 +34,7 @@ export class ContactComponent {
       'service_u7ckplf',
       'template_uzlj6ve',
       this.formData,
-      environment.emailJsPublicKey
+      'xYeiKOCfD51jS0YOT'
     ).then((result: EmailJSResponseStatus) => {
       this.statusMessage = 'Сообщение успешно отправлено!';
       this.statusType = 'success';
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    emailJsPublicKey: 'DEIN_LOKALER_KEY_HIER'
-  };
